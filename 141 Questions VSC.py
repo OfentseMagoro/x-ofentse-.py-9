5b93de8d58ce7167296325d7c5654ecf24841e46
OfentseMagoro <fefentsemarumo08@gmail.com>
2024-07-29 15:13:58 +0200

    Add files via upload

---
+++ 141 Questions VSC.py
@@ -0,0 +1,18 @@
+# QUESTION 1
+
+# What will be the value of the i variable when the while e loop finishes its execution?
+
+i=0
+while i !=0:
+    i=i-1
+else:
+    i=i=1
+print (i)
+
+# A. 1
+# B. 0
+# C. 2
+# D. the variable becomes unavailable  
+
+# the answer 1 
+# 
